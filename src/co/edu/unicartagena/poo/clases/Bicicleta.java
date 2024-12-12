08e04bd29bddee91fb970bb010f2804c80efa616
1nventa <chernandezl1@unicartagena.edu.co>
2024-12-12 12:24:30 -0500

    clase bicicleta y competencia

--- src/co/edu/unicartagena/poo/clases/Bicicleta.java
+++ src/co/edu/unicartagena/poo/clases/Bicicleta.java
@@ -6,5 +6,38 @@ package co.edu.unicartagena.poo.clases;
  * @author Carlos
  */
 public class Bicicleta {
-    
+    private String marca;
+    private String tipo;
+    private String material;
+
+    public Bicicleta(String marca, String tipo, String material) {
+        this.marca = marca;
+        this.tipo = tipo;
+        this.material = material;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
 }
+
--- src/co/edu/unicartagena/poo/clases/Competencia.java
+++ src/co/edu/unicartagena/poo/clases/Competencia.java
@@ -6,5 +6,37 @@ package co.edu.unicartagena.poo.clases;
  * @author Carlos
  */
 public class Competencia {
-    
+    private String nombre;
+    private String fecha;
+    private String lugar;
+
+    public Competencia(String nombre, String fecha, String lugar) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.lugar = lugar;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
 }
